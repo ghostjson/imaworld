c7b960d3cb196ef42576b2707105dbade4b34b76
imakhil <imakhilravindran@gmail.com>
2019-10-22 20:44:33 +0530

    project initiated

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
+	
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/login">Login</router-link>
+      <a v-on:click="logout">Logout</a>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	export default {
+		name: 'app',
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    data(){
+      return {
+
+      }
+
+    },
+    methods:{
+      logout(){
+        localStorage.removeItem('Token')
+      }
+    }
+	};
 </script>
 
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0px;
+  padding: 0px;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="login">
+		<input type="text" name="username" v-model="username"><br>
+		<input type="password" v-model="password"><br>
+		<button v-on:click="login">Submit</button>
+	</div>
+</template>
+
+<script>
+import axios from 'axios'; 
+
+export default {
+
+
+	name: 'login',
+
+	data(){
+
+		return {
+			username: '',
+			password: '',
+
+
+		}
+
+	},
+	methods: {
+		login(){
+
+
+			axios({
+			  method: 'post',
+			  url: 'http://127.0.0.1:8000/login/',
+			  data: {
+			  	'username':this.username,
+			  	'password':this.password,
+			  },
+			})
+			.then(function (response){
+				localStorage.setItem("Token",response.data)
+				console.log(localStorage.Token)
+			})
+
+			.catch(function(err){
+				console.log("Failed to login")
+			});
+		}
+	}
+
+
+
+};
+
+</script>
+
+<style scoped>
+	
+</style>
