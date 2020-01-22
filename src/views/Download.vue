9d75f62e864ff5ca7aebdd1bb257e66ae2b85800
Akhil Ravindran <imakhilravindran@gmail.com>
2020-01-22 19:50:51 +0530

    new book is added

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -3,7 +3,8 @@
 		<h2>Downloads</h2>
 
 		<div>
-			<a href="The_Beginning_of_The_Rocks.pdf" download>The Beginning of The Rocks.pdf</a>
+			<div><a href="The_Beginning_of_The_Rocks.pdf" download>The Beginning of The Rocks.pdf</a></div><br><br>
+			<div><a href="iMaWorld - Beginning of the Rocks.pdf" download>The Beginning of The Rocks (2) .pdf</a></div>
 		</div>
 	</div>
 </template>
